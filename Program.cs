5ef271b461b9aae54224e4bf4f45be466c17eadc
KellyCristinaAires <k.woys@hotmail.com>
2020-10-10 15:25:32 -0300

    atividade horasistema

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace horasistema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime horarioAtual = DateTime.Now;
+
+            Console.WriteLine("---Dia e Hora---");
+            Console.WriteLine($"---{horarioAtual}---");
+        }
+    }
+}
